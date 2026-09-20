e8f95232d8602d611821aa4d3ef957167eb6a2ad
novg <novg.novg@gmail.com>
2026-09-20 09:30:18 +0700

    Add early-init.el

---
+++ early-init.el
@@ -0,0 +1,7 @@
+;;; early-init.el --- Early startup settings -*- lexical-binding: t; -*-
+
+;; Keep package activation under init.el control.
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;;; early-init.el ends here
